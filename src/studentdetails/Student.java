cffe56aaea47819a6ede0f4e30a12bad76030582
Pamansia <pamansia@sheridancollege.ca>
2024-05-21 19:26:20 -0400

    I find social media to be a soul sucking void of meaningless affirmation

---
+++ src/studentdetails/Student.java
@@ -0,0 +1,14 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdetails;
+
+/**
+ *
+ * @author mansi
+ */
+public class Student {
+    private String name;
+    public int age=21;   
+}
